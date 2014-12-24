68d50d426372276ae5e6e76791a578ead6ac113d
Anthony Sneed <tony@tonysneed.com>
2014-12-24 14:02:13 +0100

    Added DbContext, model and controller classes.

---
+++ vNext/VS2013/EF7/NTierEf72/NTierEf7.Web/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NTierEf7.Web.Models;
+
+namespace NTierEf7.Web.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        // GET api/customers
+        [ResponseType(typeof(IEnumerable<Customer>))]
+        public async Task<IHttpActionResult> Get()
+        {
+            using (var dbContext = new NorthwindSlimContext())
+            {
+                var products = await dbContext.Customers
+                    .OrderBy(p => p.CustomerId)
+                    .ToListAsync();
+                return Ok(products);
+            }
+        }
+    }
+}
---
+++ vNext/VS2013/EF7/NTierEf72/NTierEf7.Web/Models/NorthwindSlimContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Data.Entity;
+
+namespace NTierEf7.Web.Models
+{
+    public class NorthwindSlimContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptions options)
+        {
+            options.UseSqlServer(@"data source=(local)\sqlexpress;initial catalog=NorthwindSlim;integrated security=True;pooling=False;MultipleActiveResultSets=True;");
+        }
+    }
+}
