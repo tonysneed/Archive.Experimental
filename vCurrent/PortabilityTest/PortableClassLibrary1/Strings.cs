aaf747ec261461848d5fc25ad387bdf3d694343a
Anthony Sneed <tony@tonysneed.com>
2015-02-11 14:35:02 +0100

    Added PortabilityTest.

---
+++ vCurrent/PortabilityTest/PortableClassLibrary1/Strings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+#if NET45
+    using System.ComponentModel.DataAnnotations.Schema;
+#endif
+
+namespace PortableClassLibrary1
+{
+    public class Strings
+    {
+#if NET45
+        [NotMapped]
+#endif
+        public ObservableCollection<string> Items { get; set; }
+
+#if !SILVERLIGHT
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            
+        }
+#endif
+    }
+}
